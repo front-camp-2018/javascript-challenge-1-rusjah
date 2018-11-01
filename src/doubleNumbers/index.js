3ec54eec2b3e0a599c78d58ae2b73f1cab7f367e
Ruslana Gural <rusjah1@gmail.com>
2018-11-02 00:10:09 +0200

    Implemented sortByWeight doubleNumfunctions

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,10 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const s = String(num);
+  const length = s.length;
+  if (length % 2 === 0 && s.slice(0, length / 2) === s.slice(length / 2)) {
+    return num;
+
+  }
+  return num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,21 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  return arr
+    .map(el =>
+      ({
+        el,
+        weight: el
+          .split('')
+          .reduce((acc, next) => +next + acc, 0)
+      })
+    )
+    .sort((a, b) => {
+      if (a.weight > b.weight) {
+        return 1;
+      } else if (b.weight > a.weight) {
+        return -1;
+      }
+      return 0;
+    })
+    .map(elObj => elObj.el);
 };
